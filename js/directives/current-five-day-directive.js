bffd925286aae3f19a70d321301303cc723e07a8
Jake Headley <jheadley@roomkey.com>
2017-04-08 13:51:34 -0600

    Setting up for 5 day forecast and WIP styles

---
+++ js/directives/current-five-day-directive.js
@@ -0,0 +1,10 @@
+// Start: This is the current 5 day directive ==================================
+AA.directive('currentFiveDayDirective', function () {
+
+  return {
+    restrict: 'E',
+    templateUrl: './../views/current-five-day.html',
+    controller: 'mainCtrl'
+  };
+});
+// End: This is the current 5 day directive ====================================
