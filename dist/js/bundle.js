f0e1f466736a6bfe9f3bd6891b60c8981c631b2d
Jake Headley <jheadley@roomkey.com>
2017-04-10 09:17:43 -0600

    Adding final revisions 01

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -200,29 +200,6 @@ AA.controller('mainCtrl', function ($scope, weatherService) {
 // End: This is the main controller ============================================
 'use strict';
 
-// Start: This is the Weather Service ==========================================
-AA.service('weatherService', function ($http) {
-
-  var baseUrl2 = 'http://api.openweathermap.org/data/2.5/forecast?q=';
-  var apiKey = 'c54910d1ae4d8e9e7e5bbda457d4ba22';
-
-  this.getCurrentWeather = function () {
-    return $http.get('http://ip-api.com/json').then(function (response) {
-      return response.data.city;
-    });
-  };
-  this.getWeather = function (city) {
-    console.log('at service');
-    return $http.get(baseUrl2 + city + "&appid=" + apiKey).then(function (response) {
-      console.log('got responce');
-      //console.log('This is the one:', response.data);
-      return response.data;
-    });
-  };
-});
-// End: This is the Weather Service ============================================
-'use strict';
-
 // Start: This is the current data directive ===================================
 AA.directive('currentDataDirective', function () {
 
@@ -269,4 +246,27 @@ AA.directive('headerDirective', function () {
   };
 });
 // End: This is the header directive ===========================================
+'use strict';
+
+// Start: This is the Weather Service ==========================================
+AA.service('weatherService', function ($http) {
+
+  var baseUrl2 = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+  var apiKey = 'c54910d1ae4d8e9e7e5bbda457d4ba22';
+
+  this.getCurrentWeather = function () {
+    return $http.get('http://ip-api.com/json').then(function (response) {
+      return response.data.city;
+    });
+  };
+  this.getWeather = function (city) {
+    console.log('at service');
+    return $http.get(baseUrl2 + city + "&appid=" + apiKey).then(function (response) {
+      console.log('got responce');
+      //console.log('This is the one:', response.data);
+      return response.data;
+    });
+  };
+});
+// End: This is the Weather Service ============================================
 //# sourceMappingURL=bundle.js.map
